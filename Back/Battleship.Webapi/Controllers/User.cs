a4fca52d764870a3400c01847808579809ed2550
maaIex <93136353+maaIex@users.noreply.github.com>
2022-05-14 17:25:27 +0200

    Adding db user with entity framework

---
+++ Back/Battleship.Webapi/Controllers/User.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Battleship.Webapi.Controllers
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+    }
+
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private UserDbContext _uc;
+        public UserController(UserDbContext uc)
+        {
+            _uc = uc;
+        }
+
+        [HttpGet]
+        public List<User> All()
+        {
+            return _uc.Users.ToList();
+        }
+
+        [HttpPost]
+        public User Post([FromBody] User us)
+        {
+            _uc.Users.Add(us);
+            _uc.SaveChanges();
+            return us;
+        }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext(DbContextOptions<UserDbContext> uc) : base(uc)
+        {
+
+        }
+    }
+}
--- Back/Battleship.Webapi/Program.cs
+++ Back/Battleship.Webapi/Program.cs
@@ -1,6 +1,21 @@
+using Battleship.Webapi.Controllers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContextPool<UserDbContext>(opt =>
+{
+   string cs = "Server=localhost;Port=3306;Database=battleshipUser;Uid=battleshipUser;Pwd=battleshipUser;";
+   opt.UseMySql(cs, ServerVersion.AutoDetect(cs));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
